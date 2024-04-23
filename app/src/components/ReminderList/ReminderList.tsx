383294612f1edfaa07449c18b9ff2d3c94d1eb4d
Raphael Oliveira <raphael02oliveira@gmail.com>
2024-04-22 21:29:07 -0300

    fix(app): :bug: corrige listagem de lembretes

--- app/src/components/ReminderList/ReminderList.tsx
+++ app/src/components/ReminderList/ReminderList.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import './ReminderListStyle.scss';
+import "./ReminderListStyle.scss";
 
 interface Lembrete {
   id: number;
@@ -27,13 +27,36 @@ const ListaLembretes = () => {
     <>
       <h2>Lista de lembretes:</h2>
 
-      <ul>
-        {lembretes.map((lembrete, index) => (
-          <li key={index}>
-            {lembrete.nome} - {lembrete.data}
-          </li>
-        ))}
-      </ul>
+      {lembretes.map((lembrete, index) => {
+        let agrupar = true;
+        for (let i = index - 1; i >= 0; i--) {
+          if (lembretes[i].data === lembrete.data) {
+            agrupar = false;
+            break;
+          }
+        }
+
+        if (agrupar) {
+          return (
+            <>
+              <h3>{lembrete.data}</h3>
+              <ul>
+                <li key={index}>
+                  <div className="card">{lembrete.nome}</div>
+                </li>
+              </ul>
+            </>
+          );
+        } else {
+          return (
+            <ul>
+              <li key={index}>
+                <div className="card">{lembrete.nome}</div>
+              </li>
+            </ul>
+          );
+        }
+      })}
     </>
   );
 };
